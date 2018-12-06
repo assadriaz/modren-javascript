74ec237cf41be8b2b672bbd0632dcb41e5693184
Assad Riaz <assad.riaz@bouvet.no>
2018-12-06 22:07:52 +0100

    now automate build process with npm scripts

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ import moment from 'moment';
 console.log("This is js console log...");
 console.log(moment().startOf('day').fromNow());
 
-var name = "Assad", time = "today"
+var name = "Assad", time = "today,today"
 console.log(`Hello ${name}, how are you ${time}?`);
