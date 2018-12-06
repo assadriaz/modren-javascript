4fa3305f2ae132b8258394ac522c5695aebf0131
Assad Riaz <assad.riaz@bouvet.no>
2018-12-06 22:22:49 +0100

    and finally nmp run server

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import moment from 'moment';
 
 console.log("This is js console log...");
+console.log("Anotheer log line is here");
+
 console.log(moment().startOf('day').fromNow());
 
 var name = "Assad", time = "today,today"
