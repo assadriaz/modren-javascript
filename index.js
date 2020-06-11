8f9227fc78541d4acf372b5cd0213f814288456e
Assad Riaz <assad.riaz@entur.org>
2020-06-11 09:17:43 +0200

    keep logging

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import moment from 'moment';
 
 console.log("This is js console log...");
 console.log("Anotheer log line is here");
-
+console.log("Keep loging");
 console.log(moment().startOf('day').fromNow());
 
 var name = "Assad", time = "today,today"
